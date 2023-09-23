1b1129e41d574522a9cb036e81cf76a7c0caad5a
Dave Mandelin <dmandelin@gmail.com>
2023-09-23 15:23:55 -0700

    Add a village.

--- island.ts
+++ island.ts
@@ -18,41 +18,90 @@ class Island {
     constructor(readonly w: number, readonly h: number) {
         this.tiles = [];
         for (let x = 0; x < w; ++x) {
-            const row: Tile[] = [];
+            const col: Tile[] = [];
             for (let y = 0; y < h; ++y) {
                 const isWater = x == 0 || x == w - 1 || y == 0 || y == h - 1;
-                row.push(new Tile(isWater));
+                col.push(new Tile(isWater));
             }
-            this.tiles.push(row);
+            this.tiles.push(col);
+        }
+
+        while (true) {
+            const x = randRange(0, w);
+            const y = randRange(0, h);
+            if (this.tiles[x][y].isWater) continue;
+            if (!this.tiles[x-1][y]?.isWater || this.tiles[x-1][y]?.isWater || 
+                this.tiles[x-1][y]?.isWater || this.tiles[x-1][y]?.isWater) {
+                this.tiles[x][y].addVillage(new Village(100));
+                break;
+            }
         }
     }
 }
 
 class Tile {
+    protected village_?: Village;
+
     constructor(readonly isWater) {}
+
+    addVillage(village: Village) {
+        this.village_ = village;
+    }
+
+    get village() { return this.village_; }
 }
 
-class View {
-    constructor() {
-        const svg = document.getElementById('map')!;
+class Village {
+    constructor(protected pop_: number) {
+    }
 
-        const svgNamespace = "http://www.w3.org/2000/svg";
+    get pop() { return this.pop_; }
+    protected set pop(pop: number) { this.pop_ = pop; }
+}
+
+const svgNamespace = "http://www.w3.org/2000/svg";
+
+class View {
+    protected readonly svg: HTMLElement;
+
+    constructor() {
+        this.svg = document.getElementById('map')!;
+
         const tileSize = 50;
         const borderSize = 1;
 
-        island.tiles.forEach((row, i) => {
-            row.forEach((tile, j) => {
+        island.tiles.forEach((col, x) => {
+            col.forEach((tile, y) => {
+                const rx = x * tileSize;
+                const ry = y * tileSize;
+
                 const rect = document.createElementNS(svgNamespace, 'rect');
-                rect.setAttribute('x', String(j * tileSize));
-                rect.setAttribute('y', String(i * tileSize));
+                rect.setAttribute('x', String(rx));
+                rect.setAttribute('y', String(ry));
                 rect.setAttribute('width', String(tileSize));
                 rect.setAttribute('height', String(tileSize));
                 rect.setAttribute('fill', tile.isWater ? '#AEDFF7' : '#8CB39F');
                 rect.setAttribute('stroke', 'lightgray');
                 rect.setAttribute('stroke-width', String(borderSize));
-                svg.appendChild(rect);
+                this.svg.appendChild(rect);
+
+                if (tile.village) {
+                    this.addVillageDot(rx + 23, ry + 23);
+                    this.addVillageDot(rx + 21, ry + 27);
+                    this.addVillageDot(rx + 25, ry + 27);
+                }
             });
         });
+    }
+
+    protected addVillageDot(x: number, y: number) {
+        const rect = document.createElementNS(svgNamespace, 'rect');
+        rect.setAttribute('x', String(x));
+        rect.setAttribute('y', String(y));
+        rect.setAttribute('width', '3');
+        rect.setAttribute('height', '3');
+        rect.setAttribute('fill', '#080808');
+        this.svg.appendChild(rect);
     }
 }
 
@@ -63,3 +112,7 @@ document.addEventListener('keydown', (event) => {
     if (event.key === ' ') {
     }
 });
+
+function randRange(a: number, b: number) {
+    return Math.floor(Math.random() * (b - a))
+}
